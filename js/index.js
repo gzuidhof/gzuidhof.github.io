0e36cc60c054906d1b19d7cbf986c0812aac0438
Guido Zuidhof <me@guido.io>
2023-06-15 21:52:57 +0200

    Deploy

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 'use strict';
 
+/*
+ * Handles mobile nav
+ */
+
 function toggleMobileNavState() {
   var body = document.querySelector('body');
   body.classList.toggle('nav--active');
 }
+
+/*
+ * Initializes burger functionality
+ */
 
 function initBurger() {
   var burger = document.querySelector('.burger');
